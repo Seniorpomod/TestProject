68b23983f426b47151a262afeaf7d56392eb2c9a
user <sss@gmail.com>
2024-05-31 08:26:52 +0300

    ffsdfsdfsdf

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,3 +1,12 @@
 ﻿
 
+using ConsoleApp1;
+
 Console.WriteLine("Hello, World!");
+
+Trash trash = new Trash();
+
+Trash.AddObjectToTrash(item:"табуретка");
+Trash.AddObjectToTrash(item:"попа");
+Trash.ClearTrash(); 
+Trash.AddObjectToTrash(item:"");
---
+++ ConsoleApp1/Trash.cs
@@ -0,0 +1,23 @@
+﻿namespace ConsoleApp1;
+
+public class Trash
+{
+    private IList<object> objects = new List<object>();
+    private static List<object> _objects;
+
+    public void AddprojectToTrash(object item)
+    {
+        _objects.Add(item);
+    }
+
+    public static void ClearTrash()
+    {
+        _objects = new List<object>();
+    }
+
+
+    public static void AddObjectToTrash(string item)
+    {
+        throw new NotImplementedException();
+    }
+}
